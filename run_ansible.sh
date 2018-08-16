2b5223ee4b4c5bc20f054a09e0c9601e694d5431
Wo <walter.wo@sap.com>
2018-08-16 14:30:57 +0800

    update codes

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+set -x
 Server_Name=$1
 Server_Metrix=$2
 Monitor_Time=$3
 
 cd ./ansible
 
-sudo ansible-playbook -i "${Server_Name}" ./playbook/test_script.yml -e '{"metrix":"${Server_Metrix}","runtime":"${Monitor_Time}","hosts":"${Server_Name}"}'
+sudo ansible-playbook -i "${Server_Name}" ./playbook/test_script.yml -e "{\"metrix\":\"${Server_Metrix}\",\"runtime\":\"${Monitor_Time}\",\"hosts\":\"${Server_Name}\"}"
